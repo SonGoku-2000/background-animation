8f2e489326ae9fe547067652996bd1aea5dd8651
SonGoku-2000 <pablontxs@gmail.com>
2023-04-25 14:28:00 -0500

    separar tiles de imagen

--- recortar imagen.py
+++ recortar imagen.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageChops
 import numpy as np
+import time
 
 
 def igual(img1, img2):
@@ -9,19 +10,33 @@ def igual(img1, img2):
         return True
     else:
         return False
-    
 
+
+tiles = []
+mapa = []
 
 with Image.open("animacion/mapa_prueba.bmp") as img:
+    alto = img.height//8
+    ancho = img.width//8
 
-    a = img.crop((0, 0, 8, 8))
+    for i in range(alto):
+        for j in range(ancho):
+
+            tileActual = img.crop((j*8, i*8, (j*8)+8, (i*8)+8))
+            if len(tiles) == 0:
+                tiles.append(tileActual)
+
+            for id, tile in enumerate(tiles):
+                if igual(tile,tileActual):
+                    print("coincidencia")
+                    break
+            else:
+                tiles.append(tileActual)
+            #tileActual.save(f"imagenes/{i}-{j}.bmp")
+
     b = img.crop((0, 0, 8, 8))
     c = img.crop((8, 0, 16, 8))
-
-    print(igual(a, b))
-    print(igual(a, c))
-    # img1 = img.transform((300, 300), Image.EXTENT,
-    #   data =[10, 0, 10 + img.width // 4, img.height // 3 ])
- #
-  #  img1.show()
-    # a.save("t.bmp")
+    inicio = time.time()
+    z = igual(img, img)
+    fin = time.time()
+    print(fin-inicio)
