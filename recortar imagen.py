d5b79e79895d0c7c31dc0f46be609dc6443ac2b3
SonGoku-2000 <pablontxs@gmail.com>
2023-04-25 13:11:58 -0500

    metodo para comparar imagenes creado

--- recortar imagen.py
+++ recortar imagen.py
@@ -1,22 +1,27 @@
-from PIL import Image,ImageChops
+from PIL import Image, ImageChops
 import numpy as np
 
-def igual(img1,img2):
-    img1.show()
-    img2.show()
-    c = ImageChops.subtract_modulo(img1,img2)
-    c.show()
-    pass
+
+def igual(img1, img2):
+    diferencia = ImageChops.subtract_modulo(img1, img2)
+    arr = np.asarray(diferencia)
+    if(np.count_nonzero(arr) == 0):
+        return True
+    else:
+        return False
+    
+
 
 with Image.open("animacion/mapa_prueba.bmp") as img:
 
     a = img.crop((0, 0, 8, 8))
-    b= img.crop((0, 0, 8, 8))
-    #b= img.crop((8, 0, 16, 8))
-    arr = np.asarray(c)
-    print(np.asarray(c))
-    #img1 = img.transform((300, 300), Image.EXTENT, 
+    b = img.crop((0, 0, 8, 8))
+    c = img.crop((8, 0, 16, 8))
+
+    print(igual(a, b))
+    print(igual(a, c))
+    # img1 = img.transform((300, 300), Image.EXTENT,
     #   data =[10, 0, 10 + img.width // 4, img.height // 3 ])
- # 
+ #
   #  img1.show()
-    #a.save("t.bmp")
+    # a.save("t.bmp")
